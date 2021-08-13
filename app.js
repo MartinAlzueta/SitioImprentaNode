d0264b410c823e629827fa32da3d7ee5ce3a675c
Martin Alzueta <m4lzueta@gmail.com>
2021-08-13 20:55:00 +0200

    mysql integrado

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config();
+var pool = require('./models/bd');
 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -24,13 +25,54 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
 
+// consultas:
+// seclect
+pool.query('select * from empleados').then(function (resultados) {
+  console.log(resultados);
+});
+
+// insert
+
+// var obj = {
+//   nombre: 'Cosme',
+//   apellido: 'Fulanito',
+//   trabajo: 'Programador',
+//   edad: 40,
+//   salario: 125000,
+//   mail: 'cosme@fulanito.com',
+// }
+
+// pool.query('insert into empleados set ?', [obj]).then(function (resultados) {
+//   console.log(resultados);
+// });
+
+//  update
+// var id = 2;
+// var obj = {
+//   nombre: 'Homero',
+//   apellido: 'Thompson'
+// }
+
+// pool.query('update empleados set ? where id_emp=?', [obj, id]).then(function (resultados) {
+//   console.log(resultados);
+// });
+
+// delete
+
+// var id = 17;
+
+// pool.query('delete from empleados where id_emp=?', [id]).then(function (resultados) {
+//   console.log(resultados);
+// });
+
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
